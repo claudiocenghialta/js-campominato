6b4ff7a34121a80cd90941efe751cffc457a5775
Claudio Cenghialta <claudio.cenghialta@gmail.com>
2020-07-21 16:34:43 +0200

    ook

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 
 //definisco livello di difficoltà
-var livelliDisponibili = [0, 1, 2]
-var sceltaLivello = parseInt(prompt('Inserisci il livello di difficoltà 0, 1, 2'));
-//controllo dati inseriti dall'utente
-var checkLivello = trovaElemento(livelliDisponibili, sceltaLivello)
-while (isNaN(sceltaLivello) == true || checkLivello == false) {
-    sceltaLivello = parseInt(prompt('ATTENZIONE il valore inserito non è corretto. Inserisci il livello di difficoltà 0, 1, 2'));
-    checkLivello = trovaElemento(livelliDisponibili, sceltaLivello)
+//i livelli sono dinamici, si possono aggiungere altri livelli e le relative mine
+var listaLivelli = [100, 80, 50, 5]
+var livelloMine = [16, 16, 16, 4]
+
+//chiedo scelta del livello all'utente e faccio i controlli sui dati inseriti
+var sceltaLivello = parseInt(prompt('Inserisci il livello di difficoltà tra 0 e ' + (listaLivelli.length - 1)));
+while (isNaN(sceltaLivello) == true || sceltaLivello < 0 || sceltaLivello >= listaLivelli.length) {
+    sceltaLivello = parseInt(prompt('ATTENZIONE il valore inserito non è corretto. Inserisci il livello di difficoltà tra 0 e ' + (listaLivelli.length - 1)));
 }
+var livello = listaLivelli[sceltaLivello];
+var numMine = livelloMine[sceltaLivello];
 console.log('livello scelto: ' + sceltaLivello);
-var traduciLivelli = [100, 80, 50]
-var livello = traduciLivelli[sceltaLivello];
-console.log('il livello scelto corrisponde a: ' + livello + ' numeri');
-var numMine = 16;
+console.log('il livello scelto corrisponde a: ' + livello + ' numeri ed a ' + numMine + ' mine.');
 
 //genero 16 numeri casuali x defnire le 'mine'
 var mine = [];
+var nuovoNumero;
 while (mine.length < numMine) {
-    var nuovoNumero = (random(1, livello))
-    var trovato = trovaElemento(mine, nuovoNumero);
-    if (trovato != true) {
+    nuovoNumero = (random(1, livello))
+    if (trovaElemento(mine, nuovoNumero) == false) {
         mine.push(nuovoNumero);
     }
 }
@@ -28,22 +28,24 @@ console.log('mine', mine);
 var numeriUtente = [];
 var risultato = false;
 var maxGiocate = livello - numMine;
+var nuovoNumUtente
 while (numeriUtente.length < maxGiocate && risultato == false) {
     //prompt per inserimento numeri utente
-    var nuovoNumUtente = parseInt(prompt('inserisci un numero da 1 a ' + livello));
+    nuovoNumUtente = parseInt(prompt('inserisci un numero da 1 a ' + livello));
     //controllo dati inseriti dall'utente
     while (isNaN(nuovoNumUtente) == true || nuovoNumUtente < 1 || nuovoNumUtente > livello) {
         nuovoNumUtente = parseInt(prompt('ATTENZIONE il valore inserito non è corretto. Inserisci un numero da 1 a ' + livello));
     }
     //controllo che il numero non sia già stato inserito in precedenza
-    var trovatoNumUtente = trovaElemento(numeriUtente, nuovoNumUtente);
-    if (trovatoNumUtente != true) {
+    if (trovaElemento(numeriUtente, nuovoNumUtente) == false) {
         numeriUtente.push(nuovoNumUtente);
+    } else {
+        alert('ATTENZIONE! Numero già inserito in precedenza.')
     }
     //controllo se il numero inserito dall'utente è tra le 'mine'
     risultato = trovaElemento(mine, nuovoNumUtente)
-    console.log(numeriUtente);
 }
+console.log('numeri utente', numeriUtente);
 
 //restiuisco risultati del gioco
 if (risultato == true) {
